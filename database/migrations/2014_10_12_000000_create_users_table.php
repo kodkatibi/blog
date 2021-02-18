e68a6d19ed96696efe6e29c13549f0a3ee4734f3
Ali <l3311077@std.yildiz.edu.tr>
2021-02-18 11:42:50 +0300

    added main structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role',['admin','moderator','writer','reader'])->default('reader');
             $table->rememberToken();
             $table->timestamps();
         });
