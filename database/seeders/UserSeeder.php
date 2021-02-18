f450eeb1589b470dbd4a9d5abd65e9464fd85925
Ali <l3311077@std.yildiz.edu.tr>
2021-02-18 16:45:17 +0300

    maked UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mobillium.com',
+            'password' => Hash::make('mobillium'),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Author',
+            'email' => 'writer1@mobillium.com',
+            'password' => Hash::make('mobillium'),
+            'role' => 'writer'
+        ]);
     }
 }
